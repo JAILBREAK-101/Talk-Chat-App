5945be347bea24a00a9b589512416dfb5ac43e14
JAILREAK-101 <samuelifeoluwa540@gmail.com>
2024-02-09 05:12:40 +0100

    add authentication and config screen for user log-in and sign-up - update utility styles

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,4 +1,35 @@
-// Script file for the chatapp and its logic
-const chatMessages = {
+const messagesData = {};
+
+const chatCount = document.querySelector('[data-chat-count]')
+const userImage = document.querySelector('[data-user-image]')
+const userName = document.querySelector('[data-user-name]')
+const userStatus = document.querySelector('[data-user-info]')
+const chatMessages = document.querySelector('[data-chat-messages]')
+const messageInput = document.querySelector('[data-message-input]')
+const sendButton = document.querySelector('[data-send-button]')
+
+const sendAuthData = (authDetails) => {
+    // Receive auth details needed and use in the chat application
     
-};
+}
+
+
+class ChatApp {
+    constructor(...user) {
+        this.userName = user[0]
+        this.userImage = user[1]
+    }
+
+    get test() {
+        
+    }
+
+    receiveAuthData(authData) {
+        // sendAuthData()
+    }
+}
+
+// Initialize the ChatApp Class with its methods and variables
+const TalkChatApp = new ChatApp([userName, userImage])
+
+TalkChatApp.test
