b9e05a6a1ce50e0cc601397b54f944ee0bb27761
JAILREAK-101 <samuelifeoluwa540@gmail.com>
2024-02-21 12:04:41 +0100

    add showHidePassword Function and html toggle function

--- client/src/js/auth/signin.js
+++ client/src/js/auth/signin.js
@@ -1,7 +1,27 @@
 const signInForm = document.getElementById('sign-in-form')
 
-    // Signing In - Log In
-    signInForm.addEventListener('submit', (e) => {
+const iconLabel = document.querySelector('.eye-icon-label')
+
+const showHidePassword = (passwordInput, eyeIcon) => {
+    // Toggles show and hide password for password input
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text"
+        eyeIcon.src = "./assets/slash-eye-icon.svg"
+    }
+    else {
+        passwordInput.type = "password"
+        eyeIcon.src = "./assets/show-eye-icon.svg"
+    }
+}
+
+// -- Show and Hide Password Functionality
+iconLabel.addEventListener('click', () => {
+    showHidePassword(signInForm.querySelector('#password'), iconLabel.querySelector('img'))
+})
+
+
+// Signing In - Log In
+signInForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const email = signInForm.querySelector('#email')
--- client/src/js/auth/signup.js
+++ client/src/js/auth/signup.js
@@ -1,13 +1,27 @@
-const iconLabel = document.querySelector('.eye-icon')
-
-// -- Show and Hide Password Functionality
-iconLabel.addEventListener('click', () => {
-    showHidePassword(document.getElementById('password'), iconLabel)
-})
+const iconLabels = document.querySelectorAll('.eye-icon-label')
 
 const signUpForm = document.getElementById('sign-up-form')
 
-// Signing Up - Register
+const showHidePassword = (passwordInput, eyeIcon) => {
+    // Toggles show and hide password for password input
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text"
+        eyeIcon.src = "../assets/slash-eye-icon.svg"   
+        console.log(passwordInput)
+    }
+    else {
+            passwordInput.type = "password"
+            eyeIcon.src = "../assets/show-eye-icon.svg"
+    }
+}
+
+// -- Show and Hide Password Functionality
+iconLabels.forEach(label => {
+    label.addEventListener('click', (e) => {
+        showHidePassword(signInForm.querySelector('#password'), label.querySelector('img'))
+    })
+})
+
 signUpForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
@@ -18,22 +32,17 @@ signUpForm.addEventListener('submit', (e) => {
 
     // start from validation and go to authentication then go to the sending of the data
     // if (!(validateUsername(document.querySelector('.form-group')[0]), username) && !isEmailValid(email) && !isPasswordSecure(password)) return
-
-    // else {
-
-        // For the sake of local storage
         
-        let signUpFormDetails = {
-            method: "sign_up",
-            username: username.value,
-            email: email.value,
-            password: password.value,
-            confirmPassword: confirmPassword.value
-        }
+    let signUpFormDetails = {
+        method: "sign_up",
+        username: username.value,
+        email: email.value,
+        password: password.value,
+        confirmPassword: confirmPassword.value
+    }
 
         let storedSignUpData = JSON.stringify(localStorage.setItem('user-signup-info', signUpFormDetails))
 
         authenticateUser(signUpFormDetails)
-    // }
 
 })
